37874280c21595ec69704e413fee29b44fcaed2b
Hyejoon Lim <67610661+Hyejoonlim@users.noreply.github.com>
2020-07-15 18:59:14 -0400

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Programming 
+## Week 3 Programming Assignment 2: Lexical Scoping
 
-## Write a short comment describing this function
+## This function create a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      inv <- NULL
+      set <- function(y){
+          x <<- y
+          inv <<- NULL
+      }
+      get <- function() x
+      setinverse <- function(inverse) inv <<- inverse
+      getinverse <- function () inv
+      list (set = set, get = get, 
+            setinverse = setinverse,
+            getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Computes the inverse of a square matrix returned by markCacheMatrix
+## solve() is used to compute the inverse
+## The Matrix input has to be a square matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinverse()
+        if(!is.null(inv)){
+            message ("Getting cached data")
+            return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data)
+        x$setinverse(inv)
+        inv
 }
